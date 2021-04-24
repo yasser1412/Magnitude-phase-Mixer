7e75c483f6b69c7b22dbbaed1b2e2a0a1754c34f
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2021-04-24 17:09:54 +0200

    Update FFT.cpp

--- FFT/FFT.cpp
+++ FFT/FFT.cpp
@@ -5,6 +5,30 @@
 using namespace std;
 #define pi 3.14159265359
 
+// vector<complex<double>> fft(vector<complex<double>> input)
+// {
+//     int N = input.size();
+//     vector<complex<double>> inputcopy = input;
+
+//     if(N==1) {return inputcopy;}
+
+//     int M = N/2;
+
+//     vector<complex<double>> Xeven(M,0);
+//     vector<complex<double>> Xodd(M,0);
+
+//     for(int i=0; i<M; i++)
+//     {
+//         Xeven[i] = input[2*i];
+//         Xodd[i] = input[2*i+1]; 
+//     }
+
+//     vector<complex<double>> Feven(M,0);
+//     Feven = fft(Xeven);
+//     vector<complex<double>> Fodd(M,0);
+//     Fodd = fft(Xodd);
+// }
+
 vector<complex<double>> dft(vector<complex<double>> input)
 {
     int N = input.size();
@@ -29,21 +53,26 @@ vector<complex<double>> dft(vector<complex<double>> input)
     return output;
 }
 
-main()
+main( int argc, char *argv[] )
 {
-    int N = 1000;
+    int N = atoi(argv[1]);
+
+    //int operation = atoi(argv[2]);
+
+    double phase = 0.0;
+
     vector<complex<double>> signal;
     signal.reserve(N);
 
     for(int x=0; x<N; ++x)
     {
-        auto temp = complex<double> (cos((2*pi/(1.0*N))*(x*1.0)), 0.0);
+        auto temp = complex<double> ( cos((2*pi/(1.0*N))*(x*1.0) + phase) , 0.0 );
         signal.push_back(temp);
     }
     vector<complex<double>> FreqD = dft(signal); 
     for(int i=0; i<10; i++)
     {
-        cout<<FreqD[i].real()<<"   "<<"j"<<FreqD[i].imag()<<endl;
+        cout<<FreqD[i].real()<<" + "<<"j"<<FreqD[i].imag()<<endl;
     }
 
 }
