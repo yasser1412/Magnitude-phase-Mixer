6b835f017ed6ec2d794ea5d161b445c3967118a2
khloud <61426989+Khloud-azeem@users.noreply.github.com>
2021-04-24 22:45:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,13 +12,15 @@ import sys
 import os
 import math
 from pyqtgraph import ImageView
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 import cv2 as cv
 import numpy as np
 import sys
 import os
 import qdarkgraystyle
 import logging
+
+# logging.basicConfig(filemane="logFile.txt", Level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 MAIN_WINDOW,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
 
@@ -40,8 +42,10 @@ class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
         self.msg = QMessageBox() 
 
 # list of everything
-        self.images=[]
+        self.images=[None,None]
         self.img_views=[self.imageView,self.imageView_2,self.imageView_1_edit,self.imageView_2_edit]
+        self.combos=[self.comboBox,self.comboBox_2]
+        # combo_components=['FT Magnitude','FT Phase','FT Real Component','FT Imaginary Component']
 # hide
         for i in range(len(self.img_views)):
             self.img_views[i].ui.histogram.hide()
@@ -54,7 +58,9 @@ class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
     def connect_func(self):
         self.actionImage1.triggered.connect(self.image_1)
         self.actionImage2.triggered.connect(self.image_2)
-
+        self.comboBox.currentTextChanged.connect(self.combo_1)
+        self.comboBox_2.currentTextChanged.connect(self.combo_2)
+    
     def image_1(self):
         self.img_idx = 0
         self.browse()
@@ -62,35 +68,45 @@ class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
     def image_2(self):
         self.img_idx = 1
         self.browse()
-    
+
+    def combo_1(self):
+        self.img_idx = 0
+        self.img_components()
+
+    def combo_2(self):
+        self.img_idx = 1
+        self.img_components()
+
     def browse(self):
         self.file,_ = QtGui.QFileDialog.getOpenFileName(self, 'choose the image', os.getenv('HOME') ,"Images (*.png *.xpm *.jpg)" )
         # fileName = self.file.split('/')[-1]
         if self.file == "":
             pass
-#check images size
-        #set the second argument flages=0 to draw in grayscale
-        self.image = cv.imread(self.file,0).T
+        
+        #set the second argument in imread is flages = 0 to draw in grayscale
         if self.img_idx == 0:
-            self.current_size = self.image.shape[:2]
-            self.img_views[self.img_idx].show()
-            self.img_views[self.img_idx].setImage(self.image)
-            
+            image = self.images[self.img_idx] = cv.imread(self.file,0).T
+            self.current_size = image.shape[:2]
+            self.draw_img(self.img_idx,image)
+
         elif self.img_idx == 1:
-            if self.image.shape[:2] != self.current_size:
+            image = self.images[self.img_idx] = cv.imread(self.file,0).T
+            if image.shape[:2] != self.current_size:
                 self.msg.setWindowTitle("Error in Image Size")
                 self.msg.setText("The images must have the same size")
                 self.msg.setIcon(QMessageBox.Warning)
                 x = self.msg.exec_()
                 return
             else:
-                self.img_views[self.img_idx].show()
-                self.img_views[self.img_idx].setImage(self.image)
-        self.img_components()
+                self.draw_img(self.img_idx,image)
 
-    def img_components(self):
+    def draw_img(self,idx,image):
+        self.img_views[idx].show()
+        self.img_views[idx].setImage(image)
         
-        self.dft = np.fft.fft2(self.image)
+        #error when change combo before upload photo
+    def img_components(self): 
+        self.dft = np.fft.fft2(self.images[self.img_idx])
         self.dft_shift = np.fft.fftshift(self.dft)
         self.magnitude = np.abs(self.dft_shift)
         self.magnitude_spectrum = 20*np.log(np.abs(self.dft_shift))
@@ -98,20 +114,20 @@ class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
         self.real = np.real(self.dft_shift)
         self.real_spectrum = 20*np.log(np.real(self.dft_shift))
         self.imaginary = np.imag(self.dft_shift)
-        # lsa ha check el combobox ba2a
-        if self.comboBox.currentText() == "FT Magnitude":
-            self.img_views[self.img_idx+2].show()
-            self.img_views[self.img_idx+2].setImage(self.magnitude_spectrum)
-        elif self.comboBox.currentText() == "FT Phase":
-            self.img_views[self.img_idx+2].show()
-            self.img_views[self.img_idx+2].setImage(self.phase)
-        elif self.comboBox.currentText() == "FT Real Component":
-            self.img_views[self.img_idx+2].show()
-            self.img_views[self.img_idx+2].setImage(self.real_spectrum)
-        elif self.comboBox.currentText() == "FT Imaginary Component":
-            self.img_views[self.img_idx+2].show()
-            self.img_views[self.img_idx+2].setImage(self.imaginary)
+        
+        self.check_combo()
 
+        #repitition
+    def check_combo(self):
+        if self.combos[self.img_idx].currentText() == "FT Magnitude":
+            self.draw_img(self.img_idx+2,self.magnitude_spectrum)
+        elif self.combos[self.img_idx].currentText() == "FT Phase":
+            self.draw_img(self.img_idx+2,self.phase)
+        elif self.combos[self.img_idx].currentText() == "FT Real Component":
+            self.draw_img(self.img_idx+2,self.real_spectrum)
+        elif self.combos[self.img_idx].currentText() == "FT Imaginary Component":
+            self.draw_img(self.img_idx+2,self.imaginary)
+    
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = MainApp()
